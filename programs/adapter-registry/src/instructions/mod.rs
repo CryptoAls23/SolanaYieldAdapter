36995771ab521d393e1855708641bc47ca16eab9
Oleksandr <111131497+CryptoAls23@users.noreply.github.com>
2026-06-08 10:31:54 +0300

    Create mod.rs

---
+++ programs/adapter-registry/src/instructions/mod.rs
@@ -0,0 +1,13 @@
+pub mod initialize;
+pub mod propose;
+pub mod approve;
+pub mod reject;
+pub mod deprecate;
+pub mod governance;
+
+pub use initialize::*;
+pub use propose::*;
+pub use approve::*;
+pub use reject::*;
+pub use deprecate::*;
+pub use governance::*;
