66581a4f862e9f18b78f416a44e0b4532622ecea
Oleksandr <111131497+CryptoAls23@users.noreply.github.com>
2026-06-09 10:24:45 +0300

    Create index.ts

---
+++ sdk/src/index.ts
@@ -0,0 +1,7 @@
+export { DispatcherClient } from "./dispatcher-client";
+export { RegistryClient } from "./registry-client";
+
+export * from "./constants";
+export * from "./pda";
+export * from "./types";
+export * from "./adapters/account-builders";
