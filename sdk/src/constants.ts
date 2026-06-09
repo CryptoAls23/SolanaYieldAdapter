d6a1f60bdd486c7bd299b1a38403604a5d260461
Oleksandr <111131497+CryptoAls23@users.noreply.github.com>
2026-06-09 10:25:00 +0300

    Create constants.ts

---
+++ sdk/src/constants.ts
@@ -0,0 +1,84 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const YIELD_DISPATCHER_PROGRAM_ID = new PublicKey(
+  "YieLdDiSPaTcHeRvAuLtXXXXXXXXXXXXXXXXXXXXXXX"
+);
+export const ADAPTER_REGISTRY_PROGRAM_ID = new PublicKey(
+  "AdPtReGiStRyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+);
+export const KAMINO_ADAPTER_PROGRAM_ID = new PublicKey(
+  "KAdPtKAmInOUSDCXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+);
+export const MARGINFI_ADAPTER_PROGRAM_ID = new PublicKey(
+  "MAdPtMaRGInFiUSDCXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+);
+export const JUPITER_LP_ADAPTER_PROGRAM_ID = new PublicKey(
+  "JAdPtJuPiTeRLPXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+);
+export const MAPLE_ADAPTER_PROGRAM_ID = new PublicKey(
+  "MAdPtMaPlEsYrUpXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+);
+export const DRIFT_ADAPTER_PROGRAM_ID = new PublicKey(
+  "DAdPtDrIfTiNsFuNdXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+);
+
+export const USDC_MINT = new PublicKey(
+  "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+);
+
+export const KAMINO_PROGRAM_ID = new PublicKey(
+  "KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD"
+);
+export const KAMINO_LENDING_MARKET = new PublicKey(
+  "7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF"
+);
+export const KAMINO_USDC_RESERVE = new PublicKey(
+  "D6q6wuQSriferjmtEkLeX43LchH4ix3raHmkX9W3LQba"
+);
+export const KUSDC_MINT = new PublicKey(
+  "FBSyPnxtHKLBZ4UeeUyAnbtFuAmTHLtso9YtsqRDRWkB"
+);
+
+export const MARGINFI_PROGRAM_ID = new PublicKey(
+  "MFv2hWf31Z9kbCa1snEPdcgX6CAm7cDpSvtVkGX3H5a"
+);
+export const MARGINFI_USDC_BANK = new PublicKey(
+  "2s37akK2eyBbp8DZgCm7RtsaEz8eJP3Nxd4urLHQv7yB"
+);
+export const MARGINFI_GROUP = new PublicKey(
+  "4qp6Fx6tnZkY5Wropq9wUYgtFxXKwE6viZxFHg3rdAG8"
+);
+
+export const JUPITER_PERPS_PROGRAM_ID = new PublicKey(
+  "PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu"
+);
+export const JLP_POOL = new PublicKey(
+  "5BUwFW4nRbftYTDMbgxykoFWqWHPzahFSNAaaaJtVKsq"
+);
+export const JLP_MINT = new PublicKey(
+  "27G8MtK7VtTcCHkpASjSDdkWWYfoqT6ggEuKidVJidD4"
+);
+export const JLP_USDC_CUSTODY = new PublicKey(
+  "7UUmD7TkBonBaHpJhFApjZE9B9Ftx7Vs2Nez3cFPFbVV"
+);
+
+export const MAPLE_PROGRAM_ID = new PublicKey(
+  "MaPLEd3SertEXhgnhmkMWMNUgZ3p3WGEVnJyoaJpump"
+);
+export const MAPLE_SYRUP_POOL = new PublicKey(
+  "SyRuPCkZGH3p5M4GCjLGMKZCELiHSXqtFm4nwH8Bd9R"
+);
+export const SYUSDC_MINT = new PublicKey(
+  "syUSDCEXTgrkdHJHGKiDe7PVpEWXkJuWF9Wgf7tBp4k"
+);
+
+export const DRIFT_PROGRAM_ID = new PublicKey(
+  "dRiftyHA39MWEi3m9aunc5MzRF1JYuBsbn6VPcn33UH"
+);
+export const DRIFT_STATE = new PublicKey(
+  "4aGc3mVgZTSX6qWMi4CBZpqiEzqH5DPbAuCkQKbhkCib"
+);
+export const DRIFT_IF_VAULT = new PublicKey(
+  "USDRm8LfUNNYmJAtFgvE3aqr3cxnxEPpEVEUf3G8Hjf"
+);
+export const DRIFT_USDC_SPOT_MARKET = new PublicKey(
